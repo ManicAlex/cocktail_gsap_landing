324637e79cf26db6a8c578888680dece460127d4
Alex Wiggins <alexwiggins2019@gmail.com>
2026-01-21 18:54:07 +0100

    Building of the hero page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { gsap } from 'gsap'
 import { navLinks } from '../../constants/index.jsx'
+import { useGSAP } from '@gsap/react'
 
 const NavBar = () => {
+    useGSAP(() => {
+        const navTeewn = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top',
+            }
+        });
+
+        navTeewn.fromTo('nav', {backgroundColor: 'transparent'},{
+            backgroundColor: '#00000050',
+            backgroundfilter: 'blur(10px)',
+            duration: 1,
+            ease: 'power1.inOut',
+        })
+    })
+
   return (
     <nav>
         <div>
