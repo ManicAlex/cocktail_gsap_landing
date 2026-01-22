66fe330bd07b6c27ebad085ee1ddfe96596425b4
Alex Wiggins <alexwiggins2019@gmail.com>
2026-01-22 18:14:14 +0100

    Implement cocktails section

--- constants/index.jsx
+++ constants/index.jsx
@@ -17,30 +17,30 @@ const navLinks = [
  },
 ];
 
-const cocktailLists = [
+const cockTailLists = [
  {
 	name: "Chapel Hill Shiraz",
 	country: "AU",
-	detail: "Battle",
-	price: "$10",
+	detail: "Bottle",
+	price: "€10",
  },
  {
 	name: "Caten Malbee",
 	country: "AU",
-	detail: "Battle",
-	price: "$49",
+	detail: "Bottle",
+	price: "€49",
  },
  {
 	name: "Rhino Pale Ale",
 	country: "CA",
 	detail: "750 ml",
-	price: "$20",
+	price: "€20",
  },
  {
 	name: "Irish Guinness",
 	country: "IE",
 	detail: "600 ml",
-	price: "$29",
+	price: "€29",
  },
 ];
 
@@ -48,26 +48,26 @@ const mockTailLists = [
  {
 	name: "Tropical Bloom",
 	country: "US",
-	detail: "Battle",
-	price: "$10",
+	detail: "Bottle",
+	price: "€10",
  },
  {
 	name: "Passionfruit Mint",
 	country: "US",
-	detail: "Battle",
-	price: "$49",
+	detail: "Bottle",
+	price: "€49",
  },
  {
 	name: "Citrus Glow",
 	country: "CA",
 	detail: "750 ml",
-	price: "$20",
+	price: "€20",
  },
  {
 	name: "Lavender Fizz",
 	country: "IE",
 	detail: "600 ml",
-	price: "$29",
+	price: "€29",
  },
 ];
 
@@ -171,7 +171,7 @@ const sliderLists = [
 
 export {
  navLinks,
- cocktailLists,
+ cockTailLists,
  mockTailLists,
  profileLists,
  featureLists,
